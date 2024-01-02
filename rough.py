71f6859206f7cebaebfd69e3eee0e08b3f84f2ee
emma2d <124067038+emma2d@users.noreply.github.com>
2024-01-02 22:16:04 +0000

    Delete rough.py
    Signed-off-by: emma2d <124067038+emma2d@users.noreply.github.com>

--- rough.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-# Get data frame from raw data in url.
-df = pd.read_csv("https://raw.githubusercontent.com/mwaskom/seaborn-data/master/penguins.csv")
-
-# Show.
-print(df.head())
